f03ec934d83a2f4c7b2a0673132c7e2e6dd58a73
jesuisuncaillou <igorlacroute@gmail.com>
2016-03-06 23:45:26 +0100

    wit access-token is now fetched from ENV['WIT_ACCESS_TOKEN']

--- app/models/wit_connector.rb
+++ app/models/wit_connector.rb
@@ -1,14 +1,12 @@
 class WitConnector
 	require 'wit'
 
-	ACCESS_TOKEN = "KQ2FZHZFPFZKUJ2TIUOHMUG6WJKMMBKF"
-
 	attr_accessor :wit_response
 	attr_accessor :answers
 
 	def ask(question)
 		Wit.init
-		@wit_response = Wit.text_query(question, ACCESS_TOKEN)
+		@wit_response = Wit.text_query(question, ENV["WIT_ACCESS_TOKEN"])
 		Wit.close
 		@answers = JSON.parse(@wit_response)["outcomes"]
 		return @answers.first["intent"]
